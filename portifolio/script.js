10b2a38aa92af0a9b63fc9bfa115c8e06f933564
sinkevirns <rafaelsinkevicius07@gmail.com>
2024-06-11 14:58:23 -0300

    Portifolio/Alteracao

---
+++ portifolio/script.js
@@ -0,0 +1,9 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+            document.getElementById('intro').style.display = 'none';
+        })
+        .catch(error => console.error('Error loading page:', error));
+}
